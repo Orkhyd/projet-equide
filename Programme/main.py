ea30d50163c3cfc9687398bbc4918ee20f898c9b
benoitbeckendorf <112558337+benoitbeckendorf@users.noreply.github.com>
2022-09-12 15:31:11 +0200

    Update

--- Programme/main.py
+++ Programme/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy.orm import declarative_base, relationship
 
+Base = declarative_base()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://team:*Azerty01*@localhost/projet_equides'
@@ -9,18 +12,20 @@ db.create_all()
 
 class Equides(db.Model):
     id_eq = db.Column(db.Integer, primary_key=True)
-    nom_eq = db.Column(db.String(80), unique=True, nullable=False)
-    race_eq = db.Column(db.String(120), unique=True, nullable=False)
+    nom_eq = db.Column(db.String(80))
+    race_eq = db.Column(db.String(120))
+    sexe_eq = db.Column(db.Boolean)
+    puce_eq = db.Column(db.Numeric)
+    sire_eq = db.Column(db.String(10))
 
-    def __init__(self, nom_eq, race_eq):
-        self.nom_eq = nom_eq
-        self.race_eq = race_eq
+    def __init__(self, nom_eq, race_eq, sexe_eq, puce_eq):
+         self.nom_eq = nom_eq
+         self.race_eq = race_eq
+         self.sexe_eq = sexe_eq
+         self.puce_eq = puce_eq
 
 equides = Equides.query.all()
 
-for equide in equides:
-
-   print(equide.race_eq.nom_race)
 
 @app.route('/')
 def index():
